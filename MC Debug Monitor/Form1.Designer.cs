21d1a2dab4a1866aed4613ff562fa4a55cede3fe
kemo431 <kemo14331@gmail.com>
2020-08-20 23:35:52 +0900

    プロジェクト ファイルを追加します。

---
+++ MC Debug Monitor/Form1.Designer.cs
@@ -0,0 +1,121 @@
+﻿namespace MC_Debug_Monitor
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip = new System.Windows.Forms.MenuStrip();
+            this.addTabMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.editTabMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.helpMenu = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip = new System.Windows.Forms.StatusStrip();
+            this.serverStatusLabel = new System.Windows.Forms.ToolStripStatusLabel();
+            this.menuStrip.SuspendLayout();
+            this.statusStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip
+            // 
+            this.menuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.addTabMenu,
+            this.editTabMenu,
+            this.helpMenu});
+            this.menuStrip.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip.Name = "menuStrip";
+            this.menuStrip.Size = new System.Drawing.Size(577, 24);
+            this.menuStrip.TabIndex = 0;
+            this.menuStrip.Text = "menuStrip";
+            // 
+            // addTabMenu
+            // 
+            this.addTabMenu.Name = "addTabMenu";
+            this.addTabMenu.Size = new System.Drawing.Size(71, 20);
+            this.addTabMenu.Text = "タブの追加";
+            // 
+            // editTabMenu
+            // 
+            this.editTabMenu.Name = "editTabMenu";
+            this.editTabMenu.Size = new System.Drawing.Size(71, 20);
+            this.editTabMenu.Text = "タブの編集";
+            // 
+            // helpMenu
+            // 
+            this.helpMenu.Name = "helpMenu";
+            this.helpMenu.Size = new System.Drawing.Size(48, 20);
+            this.helpMenu.Text = "ヘルプ";
+            // 
+            // statusStrip
+            // 
+            this.statusStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.serverStatusLabel});
+            this.statusStrip.Location = new System.Drawing.Point(0, 380);
+            this.statusStrip.Name = "statusStrip";
+            this.statusStrip.Size = new System.Drawing.Size(577, 24);
+            this.statusStrip.TabIndex = 1;
+            this.statusStrip.Text = "statusStrip";
+            // 
+            // serverStatusLabel
+            // 
+            this.serverStatusLabel.AutoSize = false;
+            this.serverStatusLabel.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.serverStatusLabel.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.serverStatusLabel.Margin = new System.Windows.Forms.Padding(3, 3, 0, 2);
+            this.serverStatusLabel.Name = "serverStatusLabel";
+            this.serverStatusLabel.Size = new System.Drawing.Size(60, 19);
+            this.serverStatusLabel.Text = "server";
+            this.serverStatusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 404);
+            this.Controls.Add(this.statusStrip);
+            this.Controls.Add(this.menuStrip);
+            this.MainMenuStrip = this.menuStrip;
+            this.Name = "MainForm";
+            this.Text = "MC Debug Monitor";
+            this.menuStrip.ResumeLayout(false);
+            this.menuStrip.PerformLayout();
+            this.statusStrip.ResumeLayout(false);
+            this.statusStrip.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip;
+        private System.Windows.Forms.ToolStripMenuItem addTabMenu;
+        private System.Windows.Forms.ToolStripMenuItem editTabMenu;
+        private System.Windows.Forms.ToolStripMenuItem helpMenu;
+        private System.Windows.Forms.StatusStrip statusStrip;
+        private System.Windows.Forms.ToolStripStatusLabel serverStatusLabel;
+    }
+}
+
